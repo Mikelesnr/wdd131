940b41b2c48206cf5d451060e8916fb1d0b4dda1
Mikelesnr <mwanza.n.m@gmail.com>
2024-09-25 05:08:59 +0200

    Mobile responsiveness added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,8 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 // Dynamically set the last modified date
 document.getElementById("lastModified").textContent =
   "Last Modified: " + document.lastModified;
+
+document.getElementById("menu-icon").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("show");
+});
