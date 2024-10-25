750b365f1147b4e1f36633fe5b0346c72b7c4f97
Mikelesnr <mwanza.n.m@gmail.com>
2024-10-25 23:26:47 +0200

    vehicle images added and local css and html errors fixed

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -16,11 +16,3 @@ document.addEventListener("DOMContentLoaded", function () {
         <p>© 2024 by <a href="https://www.linkedin.com/in/michael-mwanza-64469227b/" target="_blank">Michael Mwanza</a></p>
     `;
 });
-
-// document
-//   .getElementById("booking-form")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     // Validate form inputs
-//     // Update available cars based on user selection
-//   });
