3f8042cd87e2f97eb90f2b126d6b5b6ae8fbfe39
Mikelesnr <mwanza.n.m@gmail.com>
2024-10-25 21:55:35 +0200

    email form completed using email js api

--- project/scripts/mail.js
+++ project/scripts/mail.js
@@ -1,3 +1,17 @@
-function sentMail() {
-  let params = {};
-}
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("ntr-It2D3hMbyKngJ"); // my public key
+
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      emailjs.sendForm("service_mb5gexk", "template_6mx9j5l", this).then(
+        function () {
+          alert("SUCCESS!");
+        },
+        function (error) {
+          alert("FAILED...", error);
+        }
+      );
+    });
+});
